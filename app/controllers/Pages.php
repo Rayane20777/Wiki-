df4bd6fb83feba359c72206140bbaf21b6247f06
ray <ray@gmail.com>
2024-01-08 14:14:07 +0100

    routing and autoload

---
+++ app/controllers/Pages.php
@@ -0,0 +1,27 @@
+<?php
+
+class Pages extends Controller
+{
+    public function index()
+    {
+
+    }
+
+
+    public function navbar(){
+        $this->view('pages/navbar');
+    }
+
+    public function home(){
+        $this->view('pages/home');
+    }
+
+
+    public function head(){
+        $this->view('pages/head');
+    }
+   
+
+}
+?>
+
